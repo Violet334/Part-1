e94d05ce784f4c5c86b1ff95166c7a74082a190d
Violet Li <liviolet.y@gmail.com>
2024-01-11 12:19:50 -0500

    Completed Task 4b

---
+++ Assets/Week 1/Scripts/Turret.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Vertical");
+        transform.Rotate(direction * speed * Time.deltaTime, 0, 0);
+    }
+}
